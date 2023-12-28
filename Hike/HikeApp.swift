58fad7d522769acfe815939b18e0955adb47fb13
Ivan Romero <ivan_manuel_romero@hotmail.com>
2023-12-27 22:32:13 -0300

    Initial config - Added first custom view

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ivan Romero on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hike/Utility/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Ivan Romero on 27/12/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight: Color = Color("ColorGreenLight")
+    static let customGreenMedium: Color = Color("ColorGreenMedium")
+    static let customGreenDark: Color = Color("ColorGreenDark")
+    static let customGrayLight: Color = Color("ColorGrayLight")
+    static let customGrayMedium: Color = Color("ColorGrayMedium")
+    static let customIndigoMedium: Color = Color("ColorIndigoMedium")
+    static let customSalmonLight: Color = Color("ColorSalmonLight")
+}
