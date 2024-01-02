55a54fc06ce5f990a68a384327daa85cfe7b9074
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-01-02 20:57:08 -0300

    Added CustomCircleView and configured MotionAnimationView

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -63,18 +63,7 @@ struct CardView: View {
                 
                 //MARK: - MAIN CONTENT
                 ZStack {
-                    Circle()
-                        .fill(
-                            LinearGradient(
-                                colors: [
-                                    Color("ColorIndigoMedium"),
-                                    Color("ColorSalmonLight")
-                                ],
-                                startPoint: .top,
-                                endPoint: .bottomTrailing
-                            )
-                        )
-                        .frame(width: 256, height: 256)
+                    CustomCircleView()
                     Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
